3a333b62ee9f3bb50487705e385fa7e5ca9ee5fb
Shiyar Dadako <shiyar.dadako@bridging-it.de>
2023-11-30 16:14:53 +0100

    Handle Get on /home

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+N
+src/main/java/de/bit/Main.java,a\2\a28033729c9f2d91cdab25833e510caa208453bb
---
+++ src/main/java/de/bit/Main.java
@@ -0,0 +1,15 @@
+package de.bit;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/home", new RequestHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+}
