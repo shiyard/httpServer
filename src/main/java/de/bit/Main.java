ae5f296f4c40a21197935446903351a0c831a504
Shiyar Dadako <shiyar.dadako@bridging-it.de>
2023-12-07 17:10:21 +0100

    create server with new class

--- src/main/java/de/bit/Main.java
+++ src/main/java/de/bit/Main.java
@@ -1,15 +1,13 @@
 package de.bit;
 
-import com.sun.net.httpserver.HttpServer;
-
-import java.io.IOException;
-import java.net.InetSocketAddress;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/home", new RequestHandler());
+    public static void main(String[] args){
+
+        Server server = new Server(8080, 0);
+        server.createContext("/Home", new HomeRequestHandler());
         server.setExecutor(null);
         server.start();
+
+
     }
 }
