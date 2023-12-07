86850a85bf9bb9e9869d905c221711c1da979366
Shiyar Dadako <shiyar.dadako@bridging-it.de>
2023-12-07 16:46:29 +0100

    implented init methods of server. Log the operations of the server

--- src/main/java/de/bit/Server.java
+++ src/main/java/de/bit/Server.java
@@ -5,14 +5,16 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class CreateServer {
-    private int port;
-    private int backlog;
+public class Server {
+    private final int port;
+    private final int backlog;
     private HttpServer httpServer;
     private Logger log;
-    public CreateServer(int port, int backlog) {
+    public Server(int port, int backlog) {
         this.port = port;
         this.backlog = backlog;
 
@@ -20,30 +22,31 @@ public class CreateServer {
     public void create(){
         try {
             httpServer = HttpServer.create(new InetSocketAddress(port), backlog);
+            log.log(Level.INFO, "Server created on port {0}", port);
         } catch (IOException e) {
-            //handle exception
+            log.log(Level.SEVERE, e.getMessage());
         }
-
     }
 
     public void createContext(String path, HttpHandler handler) {
         httpServer.createContext(path, handler);
+        log.log(Level.INFO, "Context created with the {0} ", path);
     }
 
 
-    public int getPort() {
-        return port;
+    public void setExecutor(Executor executor) {
+        httpServer.setExecutor(executor);
     }
 
-    public void setPort(int port) {
-        this.port = port;
+    public void start (){
+        httpServer.start();
+        log.log(Level.INFO, "Server started!");
     }
 
-    public int getBacklog() {
-        return backlog;
+    public void stop(int delay) {
+        httpServer.stop(delay);
+        log.log(Level.INFO, "Server stopped!");
     }
 
-    public void setBacklog(int backlog) {
-        this.backlog = backlog;
-    }
+
 }
