f6e7e78e800b9ee39554833ee75ce9370cdb841f
Shiyar Dadako <shiyar.dadako@bridging-it.de>
2023-12-08 15:42:16 +0100

    add: Logs

--- src/main/java/de/bit/HomeRequestHandler.java
+++ src/main/java/de/bit/HomeRequestHandler.java
@@ -5,16 +5,23 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.logging.Logger;
 
 class HomeRequestHandler implements HttpHandler {
 
     private static final int RESPONSE_CODE = 200;
+    private final Logger log = Logger.getLogger(this.getClass().getName());
     @Override
-    public void handle(HttpExchange httpExchange) throws IOException {
+    public void handle(HttpExchange httpExchange) {
+        try {
         String responseMessage = "Response Message!";
         httpExchange.sendResponseHeaders(RESPONSE_CODE, responseMessage.length());
         OutputStream outputStream = httpExchange.getResponseBody();
         outputStream.write(responseMessage.getBytes());
         outputStream.close();
+
+        }catch (IOException e){
+            log.severe("Error handling  GET /Home");
+        }
     }
 }
